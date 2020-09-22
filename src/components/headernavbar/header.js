89040cd0240c086765e88aa2c398a21c5a684503
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-09-22 15:06:34 +0300

    building header and navbar

---
+++ src/components/headernavbar/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <img src='assets/images/cookbook.png'/>
+            </div>
+        )
+    }
+}
+
+export default Header;
